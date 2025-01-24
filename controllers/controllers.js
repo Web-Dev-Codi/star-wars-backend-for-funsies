28fdd8910cfee2049ae4b14191939e6a8c53b812
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-01-24 16:41:16 +0100

    get single character by Id implemented

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,7 @@
 import characterData from "../starwars.js";
 import StarWarsCharacter from "../models/starWarsCharacterModel.js";
 
-export const getAllCharacters = async (req, res, next) => {
+export const getAllCharacters = async (_req, res, _next) => {
   try {
     const allCharacters = await StarWarsCharacter.find();
 
@@ -19,7 +19,42 @@ export const getAllCharacters = async (req, res, next) => {
   }
 };
 
-export const seedCharacters = async (req, res, next) => {
+export const getCharacterById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const findCharacter = await StarWarsCharacter.findById(id);
+
+    if (!findCharacter) {
+      const error = new Error(`Could not find Character with ID ${id}`);
+      error.status = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      results: findCharacter.length,
+      data: findCharacter,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+export const getCharacterSpecies = async (req, res, _next) => {};
+
+export const getCharacterAffiliation = async (req, res, _next) => {};
+
+export const createSingleCharacter = async (req, res, _next) => {};
+
+export const updateCharacterByID = async (req, res, _next) => {};
+
+export const deleteCharacterById = async (req, res, _next) => {};
+
+export const getHighestForceRating = async (req, res, _next) => {};
+
+export const getAllJedi = async (req, res, _next) => {};
+
+export const seedCharacters = async (_req, res, _next) => {
   try {
     const addSeed = characterData.map((b) => new StarWarsCharacter(b));
     await StarWarsCharacter.insertMany(addSeed);
--- routes/routes.js
+++ routes/routes.js
@@ -2,20 +2,53 @@ import { Router } from "express";
 import {
   seedCharacters,
   getAllCharacters,
+  getCharacterById,
+  getCharacterSpecies,
+  getCharacterAffiliation,
+  createSingleCharacter,
+  updateCharacterByID,
+  deleteCharacterById,
+  getHighestForceRating,
+  getAllJedi,
 } from "../controllers/controllers.js";
 
 const router = Router();
 
 // GET all characters
+
 router.route("/").get(getAllCharacters);
+
 // GET a single character by ID
+
+router.route("/:id").get(getCharacterById);
+
 // GET characters by species
+
+router.route("/species/:species").get(getCharacterSpecies);
+
 // GET characters by affiliation
+
+router.route("/affiliation/:affiliation").get(getCharacterAffiliation);
+
 // POST create a new character
+
+router.route("/").post(createSingleCharacter);
+
 // PUT update a character by ID
+
+router.route("/:id").put(updateCharacterByID);
+
 // DELETE a character by ID
+
+router.route("/:id").delete(deleteCharacterById);
+
 // GET characters with force rating above a certain threshold
+
+router.route("/force/:force").get(getHighestForceRating);
+
 // GET all Jedi characters
+
+router.route("/jedi").get(getAllJedi);
 
 // Seed DB with Star Wars characters
 router.route("seed").post(seedCharacters);
